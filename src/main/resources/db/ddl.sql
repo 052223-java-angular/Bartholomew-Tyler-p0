b4168b05020de1db8e6d848f8fe69d9170370d76
Tyler Johnston <tylergarrettjohnston@gmail.com>
2023-05-23 16:28:55 -0400

    Initial commit

---
+++ src/main/resources/db/ddl.sql
@@ -0,0 +1,50 @@
+DROP SCHEMA IF EXISTS clip0 cascade;
+
+CREATE SCHEMA clip0;
+
+set schema 'clip0';
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS orderproducts CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
+
+CREATE TABLE users (
+    id TEXT PRIMARY key,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE products (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    price DECIMAL NOT NULL
+);
+
+CREATE TABLE orders (
+	id TEXT PRIMARY KEY,
+	user_id TEXT NOT NULL,
+	amount DECIMAL NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE orderproducts(
+	id TEXT PRIMARY KEY,
+	order_id TEXT NOT NULL,
+	product_id TEXT NOT NULL,
+	quantity INTEGER NOT NULL,
+	FOREIGN KEY (order_id) REFERENCES orders(id),
+	FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE reviews (
+    id TEXT PRIMARY KEY,
+    rating INTEGER NOT NULL,
+    comment TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    product_id TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (product_id) REFERENCES products (id)
+);
